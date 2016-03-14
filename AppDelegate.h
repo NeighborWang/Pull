72d2ce0484264234ba2f648d76d160d8c12f7d57
NeighborWang <853556067@qq.com>
2016-03-14 10:21:57 +0800

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,16 @@
+//utf-8;134217984ate.h
+//  PullBig
+//
+//  Created by lottery on 15/7/16.
+//  Copyright (c) 2015年 com.palm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
